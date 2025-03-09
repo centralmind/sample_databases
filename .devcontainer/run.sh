494a6f862641d1b3f1805bb12260f69528e9b617
Gaploid <gaploid@gmail.com>
2025-03-09 11:55:23 +0100

    changes in dev container

---
+++ .devcontainer/run.sh
@@ -0,0 +1,11 @@
+VAR1=$(gh secret list | grep VAR1 | awk '{print $2}')
+
+if [[ -z "$VAR1" ]]; then
+  echo "Error: VAR1 is not available."
+  exit 1
+fi
+
+./gateway discover --config connection.yaml --db-type postgres --ai-api-key "$VAR1" --prompt "Develop an API that enables a chatbot to retrieve information about data. Try to place yourself as analyst and think what kind of data you will require, based on that come up with useful API methods for that"
+
+unset VAR1
+history -c
