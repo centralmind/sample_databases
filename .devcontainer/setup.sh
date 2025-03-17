a0caa186dc1894394f1a28784ce0412a6d796dbe
Victor Kiselev <gaploid@gmail.com>
2025-03-17 10:36:58 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running startup commands.."
 
-wget https://github.com/centralmind/gateway/releases/download/v0.1.1/gateway-linux-amd64.tar.gz
+wget https://github.com/centralmind/gateway/releases/download/v0.0.11/gateway-linux-amd64.tar.gz
 tar -xzf gateway-linux-amd64.tar.gz
 mv gateway-linux-amd64 gateway
 chmod +x gateway
